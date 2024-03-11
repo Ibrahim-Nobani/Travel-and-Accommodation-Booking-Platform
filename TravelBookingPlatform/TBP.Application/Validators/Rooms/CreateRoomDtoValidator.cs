ee6412c86ff8da62cf4dd34c2673adc169896491
Ibrahim <inobani28@gmail.com>
2024-03-11 21:51:55 +0200

    Adding validators using fluent validation

---
+++ TravelBookingPlatform/TBP.Application/Validators/Rooms/CreateRoomDtoValidator.cs
@@ -0,0 +1,33 @@
+using FluentValidation;
+using TravelBookingPlatform.Application.DTOs;
+
+public class CreateRoomDtoValidator : AbstractValidator<CreateRoomDto>
+{
+    public CreateRoomDtoValidator()
+    {
+        RuleFor(room => room.Number)
+            .NotEmpty().WithMessage("Room number is required.")
+            .GreaterThan(0).WithMessage("Room number must be greater than 0.");
+
+        RuleFor(room => room.Price)
+            .NotEmpty().WithMessage("Price is required.")
+            .GreaterThan(0).WithMessage("Price must be greater than 0.");
+
+        RuleFor(room => room.ThumbnailImageUrl)
+            .NotEmpty().WithMessage("Thumbnail image URL is required.")
+            .MaximumLength(255).WithMessage("Thumbnail image URL cannot exceed 255 characters.")
+            .Must(url => Uri.TryCreate(url, UriKind.Relative, out _))
+            .WithMessage("Thumbnail image URL must be a valid relative URL.");
+
+        RuleFor(room => room.AdultCapacity)
+            .NotEmpty().WithMessage("Adult capacity is required.")
+            .GreaterThan(0).WithMessage("Adult capacity must be greater than 0.");
+
+        RuleFor(room => room.ChildCapacity)
+            .NotEmpty().WithMessage("Child capacity is required.")
+            .GreaterThanOrEqualTo(0).WithMessage("Child capacity must be greater than or equal to 0.");
+
+        RuleFor(room => room.Availability)
+            .NotNull().WithMessage("Availability is required.");
+    }
+}
